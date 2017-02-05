9581dbf0a81b7da12f03cf37ace5ba6bd70ff5d6
Anastasia Tarkanii <nastyatrko@gmail.com>
2017-02-05 12:32:32 +0200

    Implementation of compute_pdf method

--- FSProcessor.py
+++ FSProcessor.py
@@ -1,6 +1,7 @@
 import math
 import numpy as np
 import cv2
+import time
 
 
 KERNEL_GAUSSIAN = 0
@@ -20,12 +21,15 @@ class FSProcessor:
         self.domains = [dim for dim in zip(domain_type, domain_dim, domain_h)]
 
         self.kernel = FSProcessor.undefined
+        self.kernel_type = FSProcessor.undefined
         if not kernel:
             self.define_kernel(KERNEL_EPANECHNIKOV)
         else:
             self.define_kernel(kernel)
 
         self.img = FSProcessor.undefined
+        self.N = FSProcessor.undefined
+        self.pdf = FSProcessor.undefined
 
     def define_kernel(self, kernel_type):
         if kernel_type == KERNEL_GAUSSIAN:
@@ -34,31 +38,82 @@ class FSProcessor:
                 return pow(2 * math.pi, dimensions / -2) * math.exp(-0.5 * dist ** 2)
 
             self.kernel = lambda dist, dimension, h: gaussian(dist, dimension)
+            self.kernel_type = KERNEL_GAUSSIAN
         elif kernel_type == KERNEL_EPANECHNIKOV:
-
+            # Called only when dist < 1
             def epanechnikov(dist, dimension, h):
-                if dist < 1:
-                    c = 1
-                    if dimension == 2:
-                        c = math.pi * h ** 2
-                    elif dimension == 3:
-                        c = 4 * math.pi * h ** 3 / 3
-                    else:
-                        # TODO custom c
-                        pass
+                c = 1
+                if dimension == 2:
+                    c = math.pi * h ** 2
+                elif dimension == 3:
+                    c = 4 * math.pi * h ** 3 / 3
+                else:
+                    # TODO custom c
+                    pass
 
-                    return c * (dimension + 2) * (1 - dist ** 2) / 2
-                else: return 0
+                return (dimension + 2) * (1 - dist ** 2) / (2 * c)
 
             self.kernel = lambda dist, dimension, h: epanechnikov(dist, dimension, h)
+            self.kernel_type = KERNEL_EPANECHNIKOV
         else:
             raise ValueError("Kernel type is not understood")
 
+    def find_dist(self, coord1, coord2):
+        dist = 0
+        for axis1, axis2 in zip(coord1, coord2):
+            dist += (axis1 - axis2) ** 2
+        return math.sqrt(dist)
+
+    def compute_kernel(self, coord1, coord2, domain_dim, domain_h):
+        kdf = 0
+        dist = self.find_dist(coord1, coord2)
+
+        if self.kernel_type == KERNEL_EPANECHNIKOV:
+            if dist < domain_h: kdf = self.kernel(dist / domain_h, domain_dim, domain_h)
+        else:
+            kdf = self.kernel(dist / domain_h, domain_dim, domain_h)
+
+        return kdf
+
+    def compute_pdf(self):
+        print(self.N)
+        self.pdf = np.empty((self.img.shape[0], self.img.shape[1]), dtype=np.float32)
+
+        for y in range(self.img.shape[0]):
+            for x in range(self.img.shape[1]):
+                start = time.time()
+                # TODO estimate normalized constant that makes K(x) integrate to one
+                kernel_sum = 0
+
+                for _y in range(self.img.shape[0]):
+                    for _x in range(self.img.shape[1]):
+                        composite_kernel = 1
+
+                        for _type, dim, h in self.domains:
+                            coord1, coord2 = {
+                                SPATIAL_DOMAIN: ((y,x), (_y, _x)),
+                                RANGE_DOMAIN: (self.img[y,x], self.img[_y, _x])
+                            }[_type]
+
+                            composite_kernel *= self.compute_kernel(coord1, coord2, dim, h)
+                            composite_kernel /= h ** dim
+
+                        kernel_sum += composite_kernel
+
+                self.pdf[y,x] = kernel_sum / self.N
+                print(time.time() - start)
+
+    def write_results(self):
+        for y in range(self.img.shape[0]):
+            for x in range(self.img.shape[1]):
+                print(y, x, self.pdf[y, x])
 
     def load_img(self, file):
         self.img = np.float32(cv2.imread(file))
         self.img *= 1./255
         self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2Luv)
+
+        self.N = self.img.shape[0] * self.img.shape[1]
 
     @staticmethod
     def get_undefined_object():
--- __init__.py
+++ __init__.py
@@ -1,6 +1,9 @@
-from FSProcessor import FSProcessor
+from FSProcessor import FSProcessor, SPATIAL_DOMAIN, RANGE_DOMAIN
 
 
 if __name__ == '__main__':
-    processor = FSProcessor(2, (2, 3))
-    processor.load_img('../../food.jpg')
+    processor = FSProcessor(2, (SPATIAL_DOMAIN, RANGE_DOMAIN), (2, 3), (8, 8))
+    processor.load_img('../../../Python/cv/images/logo.jpg')
+
+    processor.compute_pdf()
+    processor.write_results()
