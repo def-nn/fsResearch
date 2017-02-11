76a2cfba564286fcd56f59eaa1a7bfed3f736f9b
Anastasia Tarkanii <nastyatrko@gmail.com>
2017-02-11 20:50:51 +0200

    Finding local maxima

--- __init__.py
+++ __init__.py
@@ -4,11 +4,13 @@ from FSProcessor import FSProcessor, SPATIAL_DOMAIN, RANGE_DOMAIN
 
 if __name__ == '__main__':
     processor = FSProcessor(2, (SPATIAL_DOMAIN, RANGE_DOMAIN), (2, 3), (8, 8))
-    processor.load_img('../../../Python/cv/images/woman_face.jpg')
+    # processor.load_img('../../../Python/cv/images/woman_face.jpg')
+    #
+    # start = time.time()
+    # processor.compute_pdf()
+    # t1 = time.time()
+    # print(t1 - start)
+    # processor.write_results('/Users/Anastasia/Documents/work/pdf_face.txt')
+    # print(time.time() - t1)
+    processor.find_local_max('/Users/Anastasia/Python/cv/images/food1.jpg')
 
-    start = time.time()
-    processor.compute_pdf()
-    t1 = time.time()
-    print(t1 - start)
-    processor.write_results('/Users/Anastasia/Documents/work/pdf_face.txt')
-    print(time.time() - t1)
